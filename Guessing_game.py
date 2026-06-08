dbe9f39b802746d8bc083494a9ae0f772e12e7f0
Diego Bolzan <dbolzan.dev@gmail.com>
2026-06-08 12:43:04 -0300

    Guessing_game.py

--- Guessing_game.py
+++ Guessing_game.py
@@ -3,11 +3,7 @@ attempts = []
 guesses = 0
 chances = 0
 divider = '------------------------------'
-try:
-     difficulty = int(input('\tChoose a difficulty level!\n[1] Easy (1-10)\n[2] Medium (1-50)\n[3] Hard (1-100)\n'))
-except ValueError:
-     print('Please select a valid option.')
-continue
+difficulty = int(input('\tChoose a difficulty level!\n[1] Easy (1-10)\n[2] Medium (1-50)\n[3] Hard (1-100)\n'))
 if difficulty == 1:
      random_number = random.randint(1,10)
 elif difficulty == 2:
@@ -15,11 +11,7 @@ elif difficulty == 2:
 elif difficulty == 3:
     random_number = random.randint(1, 100)
 while guesses != random_number and chances < 5:
-    try:
-         guesses = int(input(f'{divider}\nGuess a number: '))
-    except ValueError:
-         print('Please select a valid value.')
-    continue
+    guesses = int(input(f'{divider}\nGuess a number: '))
     attempts.append(guesses)
     chances += 1
     if guesses > random_number:
