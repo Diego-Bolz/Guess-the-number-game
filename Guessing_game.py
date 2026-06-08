b37b24521d45aa46477ca446e5f3c6143961129e
Diego Bolzan <dbolzan.dev@gmail.com>
2026-06-08 13:16:26 -0300

    Guessing_game.py

--- Guessing_game.py
+++ Guessing_game.py
@@ -3,7 +3,17 @@ attempts = []
 guesses = 0
 chances = 0
 divider = '------------------------------'
-difficulty = int(input('\tChoose a difficulty level!\n[1] Easy (1-10)\n[2] Medium (1-50)\n[3] Hard (1-100)\n'))
+difficulty = 0
+while difficulty == 0:
+    try:
+       difficulty = int(input('\tChoose a difficulty level!\n[1] Easy (1-10)\n[2] Medium (1-50)\n[3] Hard (1-100)\n'))
+    except ValueError:
+        print('Please insert only integer values')
+    if dificuldade in [1, 2, 3]:
+        break
+    else:
+        print('Insert a valid value. (1, 2 or 3)')
+        dificuldade = 0
 if difficulty == 1:
      random_number = random.randint(1,10)
 elif difficulty == 2:
@@ -11,9 +21,12 @@ elif difficulty == 2:
 elif difficulty == 3:
     random_number = random.randint(1, 100)
 while guesses != random_number and chances < 5:
-    guesses = int(input(f'{divider}\nGuess a number: '))
-    attempts.append(guesses)
-    chances += 1
+     try:
+          guesses = int(input(f'{divider}\nGuess a number: '))
+          attempts.append(guesses)
+          chances += 1
+     except ValueError:
+          print('Please insert only integer values')
     if guesses > random_number:
         print(f'Lower!')
     elif guesses < random_number:
